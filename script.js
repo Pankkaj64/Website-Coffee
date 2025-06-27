aa8ab177ce86261f91159674765cb1014a1a6e24
Pankaj Yadav <pankkaj@Pankajs-MacBook-Pro.local>
2025-06-27 17:49:18 +0400

    Completed Projects

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const menuOpenButton = document.querySelector("#menu-open-button")
-const menuCloseButton = document.querySelector("#menu-close-button")
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector('#menu-close-button');
 
 
 menuOpenButton.addEventListener("click", () => {
@@ -9,6 +10,13 @@ menuOpenButton.addEventListener("click", () => {
 
 //Initialize Swiper
 menuCloseButton.addEventListener('click',() => menuOpenButton.click())
+
+// Close menu when the close button is clicked
+navLinks.forEach(link => {
+    link.addEventListener( "click", () => menuOpenButton.click())
+   
+})
+
 
 const swiper = new Swiper('.slider-wrapper', {
   loop: true,
